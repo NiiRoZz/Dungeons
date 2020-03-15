24aaa64d40f4db0e1f8dc3d2b478299aaf2911e2
NiiRoZz <extazziii1000@gmail.com>
2020-03-15 13:27:23 +0100

    Update glad and link to dl library if not on windows

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,17 @@
-#define GLAD_GL_IMPLEMENTATION
-#include "glad/gl.h"
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#include <glm/matrix.hpp>
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
 static const GLfloat vertices[] = {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   0.0f,  1.0f, 0.0f,
+  -0.5f, -0.5f, 0.0f,
+  0.5f, -0.5f, 0.0f,
+  0.0f,  0.5f, 0.0f
 };
 
 static void error_callback([[maybe_unused]] int error, const char* description) {
@@ -19,12 +25,108 @@ static void key_callback(GLFWwindow* window, int key, [[maybe_unused]] int scanc
   }
 }
 
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path) {
+
+	// Create the shaders
+	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Read the Vertex Shader code from the file
+	std::string VertexShaderCode;
+	std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+	if (VertexShaderStream.is_open()) {
+		std::stringstream sstr;
+		sstr << VertexShaderStream.rdbuf();
+		VertexShaderCode = sstr.str();
+		VertexShaderStream.close();
+	} else {
+		fprintf(stderr, "Impossible to open %s.\n", vertex_file_path);
+		abort();
+	}
+
+	// Read the Fragment Shader code from the file
+	std::string FragmentShaderCode;
+	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+	if (FragmentShaderStream.is_open()) {
+		std::stringstream sstr;
+		sstr << FragmentShaderStream.rdbuf();
+		FragmentShaderCode = sstr.str();
+		FragmentShaderStream.close();
+	} else {
+    fprintf(stderr, "Impossible to open %s.\n", fragment_file_path);
+		abort();
+  }
+
+	GLint Result = GL_FALSE;
+	int InfoLogLength;
+
+	// Compile Vertex Shader
+	char const * VertexSourcePointer = VertexShaderCode.c_str();
+	glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
+	glCompileShader(VertexShaderID);
+
+	// Check Vertex Shader
+	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if ( InfoLogLength > 0 ){
+		std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
+		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+		fprintf(stderr, "%s\n", &VertexShaderErrorMessage[0]);
+    abort();
+	}
+
+	// Compile Fragment Shader
+	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
+	glCompileShader(FragmentShaderID);
+
+	// Check Fragment Shader
+	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if ( InfoLogLength > 0 ){
+		std::vector<char> FragmentShaderErrorMessage(InfoLogLength+1);
+		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+		fprintf(stderr, "%s\n", &FragmentShaderErrorMessage[0]);
+    abort();
+	}
+
+	// Link the program
+	GLuint ProgramID = glCreateProgram();
+	glAttachShader(ProgramID, VertexShaderID);
+	glAttachShader(ProgramID, FragmentShaderID);
+	glLinkProgram(ProgramID);
+
+	// Check the program
+	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if ( InfoLogLength > 0 ){
+		std::vector<char> ProgramErrorMessage(InfoLogLength+1);
+		glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+		fprintf(stderr, "%s\n", &ProgramErrorMessage[0]);
+    abort();
+	}
+
+	glDetachShader(ProgramID, VertexShaderID);
+	glDetachShader(ProgramID, FragmentShaderID);
+	
+	glDeleteShader(VertexShaderID);
+	glDeleteShader(FragmentShaderID);
+
+	return ProgramID;
+}
+
 int main() {
-  glfwSetErrorCallback(error_callback);
-  
   /* Initialize the library */
   if (!glfwInit())
     return -1;
+
+  glfwSetErrorCallback(error_callback);
+
+  glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   /* Create a windowed mode window and its OpenGL context */
   GLFWwindow* window = glfwCreateWindow(640, 480, "Dungeons", NULL, NULL);
@@ -37,8 +139,19 @@ int main() {
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
-  gladLoadGL(glfwGetProcAddress);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    fprintf(stderr, "Unable to initialize glad\n");
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return -2;
+  }
+
   glfwSwapInterval(1);
+
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
 
   GLuint vertex_buffer;
   glGenBuffers(1, &vertex_buffer);
@@ -56,10 +169,17 @@ int main() {
     (void*)0            // array buffer offset
   );
 
+  GLuint programID = LoadShaders( "../shaders/SimpleVertexShader.glsl", "../shaders/SimpleFragmentShader.glsl" );
+  glUseProgram(programID);
+
+  //glEnable(GL_CULL_FACE);
+  //glEnable(GL_DEPTH_TEST);
+  //glDepthFunc(GL_LESS);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
     glDrawArrays(GL_TRIANGLES, 0, 3);
